448f48eb50d10465f0ff0499f727648fbdc2bb4d
Ivan Martynenko <iwan@tversu.ru>
2025-10-15 02:29:21 +0300

    add material

--- bin/my_maya_convertor.rb
+++ bin/my_maya_convertor.rb
@@ -6,7 +6,7 @@ FPS = 24.0
 DEG2RAD = Math::PI / 180.0
 
 def fmt_f(x)
-  ('%.9f' % x.to_f).sub(/\.?0+$/, '') # компактно без лишних нулей
+  format('%.9f', x.to_f).sub(/\.?0+$/, '') # компактно без лишних нулей
 end
 
 class NmfJsonToMaya
@@ -21,10 +21,10 @@ class NmfJsonToMaya
       parent_name = parent_node ? parent_node[:name] : nil
       parent_name = nil if node[:parent_iid] == 1
 
-    case node[:word]
-      when 'JOIN'
-        @result.push MayaFrameObject.new(unpacked_node, node_name, parent_name, as_joint: true)
+      case node[:word]
       when 'FRAM'
+        @result.push MayaFrameObject.new(unpacked_node, node_name, parent_name, as_joint: true)
+      when 'JOIN'
         @result.push MayaFrameObject.new(unpacked_node, node_name, parent_name, as_joint: true)
       when 'MESH'
         @result.push MayaMeshObject.new(unpacked_node, node_name, parent_name)
@@ -38,33 +38,38 @@ class NmfJsonToMaya
 end
 
 class MayaBaseObject
+  def to_s
+    ''
+  end
 end
 
 class MayaFrameObject < MayaBaseObject
   def initialize(fram_data, node_name = 'transformNode', parent_node_name, as_joint: true)
     @node_name = node_name
     @parent_node_name = parent_node_name
-    @translation = fram_data[:translation] || [0,0,0]
-    @scaling     = fram_data[:scaling]     || [1,1,1]
-    @rotation    = fram_data[:rotation]    || [0,0,0] # radians в базовом значении
+    @translation = fram_data[:translation] || [0, 0, 0]
+    @scaling     = fram_data[:scaling]     || [1, 1, 1]
+    @rotation    = fram_data[:rotation]    || [0, 0, 0] # radians в базовом значении
 
-    @rpv_t       = fram_data[:rotate_pivot_translate] || [0,0,0]
-    @rpv         = fram_data[:rotate_pivot]           || [0,0,0]
-    @spv_t       = fram_data[:scale_pivot_translate]  || [0,0,0]
-    @spv         = fram_data[:scale_pivot]            || [0,0,0]
-    @shear       = fram_data[:shear]                  || [0,0,0]
+    @rpv_t       = fram_data[:rotate_pivot_translate] || [0, 0, 0]
+    @rpv         = fram_data[:rotate_pivot]           || [0, 0, 0]
+    @spv_t       = fram_data[:scale_pivot_translate]  || [0, 0, 0]
+    @spv         = fram_data[:scale_pivot]            || [0, 0, 0]
+    @shear       = fram_data[:shear]                  || [0, 0, 0]
 
     @anim        = fram_data[:anim] || {}
 
-    rad2deg = ->(a){ a * 180.0 / Math::PI }
+    rad2deg = ->(a) { a * 180.0 / Math::PI }
     @rot_deg = @rotation.map { |r| rad2deg.call(r) }
   end
 
   def to_s
-    str  = +"\n"
-    header = @parent_node_name ?
-      "createNode transform -name \"#{@node_name}\" -parent \"#{@parent_node_name}\";" :
-      "createNode transform -name \"#{@node_name}\";"
+    str = +"\n"
+    header = if @parent_node_name
+               "createNode transform -name \"#{@node_name}\" -parent \"#{@parent_node_name}\";"
+             else
+               "createNode transform -name \"#{@node_name}\";"
+             end
     str << header
     # базовые TRS (статические значения узла)
     str << "\n\tsetAttr \".translate\" -type \"double3\" #{@translation.join(' ')};"
@@ -94,26 +99,23 @@ class MayaFrameObject < MayaBaseObject
     # @anim[:translation][:values][:x] = [v0, v1,...]
     # @anim[:translation][:keys][:x]   = [t0, t1,...]  (секунды)
     {
-      translation: {curve: 'animCurveTL', attrs: %w[translateX translateY translateZ], axes: %i[x y z]},
-      rotation:    {curve: 'animCurveTA', attrs: %w[rotateX rotateY rotateZ],         axes: %i[x y z]},
-      scaling:     {curve: 'animCurveTU', attrs: %w[scaleX scaleY scaleZ],            axes: %i[x y z]}
+      translation: { curve: 'animCurveTL', attrs: %w[translateX translateY translateZ], axes: %i[x y z] },
+      rotation: { curve: 'animCurveTA', attrs: %w[rotateX rotateY rotateZ], axes: %i[x y z] },
+      scaling: { curve: 'animCurveTU', attrs: %w[scaleX scaleY scaleZ], axes: %i[x y z] }
     }.each do |track, spec|
       next unless @anim[track]
+
       values = @anim[track][:values] || {}
       keys   = @anim[track][:keys]   || {}
 
       spec[:axes].each_with_index do |ax, i|
-        vlist = (values[ax] || values[ax.to_s] || [])
-        tlist = (keys[ax]   || keys[ax.to_s]   || [])
+        vlist = values[ax] || values[ax.to_s] || []
+        tlist = keys[ax]   || keys[ax.to_s]   || []
         next if vlist.nil? || tlist.nil? || vlist.empty? || tlist.empty?
 
         # перевод секунд -> кадры
         frames = tlist.map { |t| (t.to_f * FPS) }
-        if track === :rotation
-          puts "#{ax} #{vlist}"
-          vlist.map! { |v| v / DEG2RAD }
-          puts "#{vlist}"
-        end
+        vlist.map! { |v| v / DEG2RAD } if track == :rotation
 
         curve_name = "#{@node_name}_#{spec[:attrs][i]}"
         out << build_anim_curve(spec[:curve], curve_name, frames, vlist)
@@ -141,10 +143,10 @@ class MayaFrameObject < MayaBaseObject
     return '' if n == 0
 
     pairs = (0...n).map { |i| "#{fmt_f(frames[i])} #{fmt_f(values[i])}" }.join(' ')
-    s  = +"\ncreateNode #{curve_type} -name \"#{curve_name}\";"
+    s  = "\ncreateNode #{curve_type} -name \"#{curve_name}\";"
     s << "\n\tsetAttr \".tangentType\" 9;"
     s << "\n\tsetAttr \".weightedTangents\" no;"
-    s << "\n\tsetAttr -size #{n} \".keyTimeValue[0:#{n-1}]\" #{pairs};"
+    s << "\n\tsetAttr -size #{n} \".keyTimeValue[0:#{n - 1}]\" #{pairs};"
     # при желании можно задать выходные типы тангенсов:
     # s << "\n\tsetAttr -size #{n} \".keyTanOutType[0:#{n-1}]\" #{Array.new(n, 5).join(' ')};"
     s
@@ -155,7 +157,6 @@ class MayaFrameObject < MayaBaseObject
   end
 end
 
-
 class MayaMeshObject < MayaBaseObject
   def initialize(mesh_data, node_name = 'meshShape', parent_node_name = 'meshTransform')
     @node_name = node_name
@@ -164,185 +165,201 @@ class MayaMeshObject < MayaBaseObject
     # Вершины (xyz)
     @vrts = mesh_data[:vbuf].map { |t| [t[0], t[1], t[2]] }
 
-    # Исходные треугольники (по вершинам) — пригодятся для mf
-    @ibuf = mesh_data[:ibuf].map { |tri| [tri[0], tri[2], tri[1]] } # [v1,v2,v3]
+    # Исходные треугольники
+    @ibuf = mesh_data[:ibuf].map { |tri| [tri[0], tri[1], tri[2]] }
 
-    # Сгенерим рёбра и индексы рёбер для polyFaces::f
-    # @edge = generate_edges_from_ibuf(@ibuf)
-    # @face = generate_faces_from_edges_an_ibuf(@edge, @ibuf)
+    # Рёбра и faces с подписанным направлением для polyFaces::f
     @edge, @face = build_edges_and_faces_signed(@ibuf)
-     @edge.map! { |e| e.length == 2 ? e + [0] : e }
+    @edge.map! { |e| e.length == 2 ? e + [0] : e }
 
-    # ---------- UV: строим пул uvpt и соответствие вершина->uv-индекс ----------
-    # Приоритет: mesh_data[:uvpt] если длина == числу вершин; иначе берём из vbuf[6,7]; иначе 0,0
-    uv_source_per_vertex =
-      if mesh_data[:uvpt].is_a?(Array) && mesh_data[:uvpt].length == @vrts.length
-        mesh_data[:uvpt].map { |u| [u[0].to_f, u[1].to_f] }
-      else
-        mesh_data[:vbuf].map { |t| [(t[6] || 0.0).to_f, (t[7] || 0.0).to_f] }
-      end
+    @materials = mesh_data[:materials]
 
-    # В этой сборке лоадер отлично работает, когда uvpt идёт "по-вершинно":
-    # uv-индекс == индекс вершины. Это же сильно упрощает mf.
-    @uvpt = uv_source_per_vertex
+    # ---------- UV: используем ТОЛЬКО vbuf[6,7] (пер-вершинно), сохраняем >1.0 ----------
+    # # FIX: приоритет vbuf, никаких замен на mesh_data[:uvpt]
+    @uvpt = mesh_data[:vbuf].map do |t|
+      u = (t[6] || 0.0).to_f
+      v = (t[7] || 0.0).to_f
+      [u, v]
+    end
+
+    # Индекс UV == индекс вершины (то, что ожидает обратный экспортёр)
     @uv_index_of_vertex = (0...@vrts.length).to_a
+
+    @materials = mesh_data[:materials].map do |m|
+      mat_name = m[:name]
+      mat_name = "lambert_#{@node_name}" if mat_name == '' || mat_name.empty?
+      puts m[:alpha]
+      a = (m[:alpha] || 1.0).to_f
+      {
+        mat_name:,
+        sg_name: "#{mat_name}SG",
+        r: (m[:red]   || 1).to_f,
+        g: (m[:green] || 1).to_f,
+        b: (m[:blue]  || 1).to_f,
+        a:,
+        t: a, # прозрачность в Maya: 0..1, где 1 — полностью прозрачно
+        # UV placement из твоих полей
+        repeatU: (m[:horizontal_stretch] || 1).to_i,
+        repeatV: (m[:vertical_stretch]   || 1).to_i,
+        mirrorU: (m[:uv_mapping_flip_horizontal] || 0).to_i == 1 ? 1 : 0,
+        mirrorV: (m[:uv_mapping_flip_vertical]   || 0).to_i == 1 ? 1 : 0,
+        rotateUV: (m[:rotate] || 0).to_i # если у тебя это «четверть-обороты», умножь на 90
+      }
+    end
   end
 
   # Построение неориентированных рёбер + ПОДПИСАННЫЕ индексы рёбер для f
-def build_edges_and_faces_signed(tris)
-  edge_map = {}   # "va|vb" -> edge_index
-  edges    = []   # [[va,vb], ...] с va < vb
+  def build_edges_and_faces_signed(tris)
+    edge_map = {}   # "va|vb" -> edge_index
+    edges    = []   # [[va,vb], ...] с va < vb
 
-  # 1) Собираем уникальные рёбра
-  tris.each do |(a, b, c)|
-    [[a, b], [b, c], [c, a]].each do |u, v|
-      va, vb = [u, v].minmax
-      key = "#{va}|#{vb}"
-      unless edge_map.key?(key)
-        edge_map[key] = edges.length
-        edges << [va, vb]
-      end
-    end
-  end
-
-  # 2) Для каждого треугольника считаем ПОДПИСАННЫЕ индексы рёбер (для f)
-  faces_signed = tris.map do |(a, b, c)|
-    e0 = signed_edge_index(edge_map, a, b) # (a→b)
-    e1 = signed_edge_index(edge_map, b, c) # (b→c)
-    e2 = signed_edge_index(edge_map, c, a) # (c→a)
-    [e0, e1, e2]
-  end
-
-  [edges, faces_signed]
-end
-
-# Возвращает индекс ребра с направлением:
-#   совпало с хранением (va<vb и (a,b)==(va,vb))  -> idx
-#   обратное направление                          -> -(idx+1)
-def signed_edge_index(edge_map, a, b)
-  va, vb = [a, b].minmax
-  idx = edge_map["#{va}|#{vb}"]
-  raise "edge not found for #{a}-#{b}" unless idx
-  same_dir = (a == va) && (b == vb)
-  same_dir ? idx : -(idx + 1)
-end
-
-  def build_edges_and_faces(tris)
-    edge_map = {}      # "va|vb" -> edge_index
-    edges = []         # [[va,vb,0], ...]
-    faces = []         # [[e0,e1,e2], ...]
-
+    # 1) Собираем уникальные рёбра
     tris.each do |(a, b, c)|
-      tri_edges = [[a, b], [b, c], [c, a]].map do |u, v|
+      [[a, b], [b, c], [c, a]].each do |u, v|
         va, vb = [u, v].minmax
         key = "#{va}|#{vb}"
-        idx = edge_map[key]
-        unless idx
-          idx = edges.length
-          edge_map[key] = idx
-          edges << [va, vb, 0]  # 3-й элемент — как у вас, флаг/сглаживание/и пр.
+        unless edge_map.key?(key)
+          edge_map[key] = edges.length
+          edges << [va, vb]
         end
-        idx
       end
-      faces << tri_edges
     end
 
-    faces.map! { |t| [t[0], t[1], t[2]] }
-    [edges, faces]
+    # 2) Подписанные индексы рёбер для каждого треугольника
+    faces_signed = tris.map do |(a, b, c)|
+      e0 = signed_edge_index(edge_map, a, b) # (a→b)
+      e1 = signed_edge_index(edge_map, b, c) # (b→c)
+      e2 = signed_edge_index(edge_map, c, a) # (c→a)
+      [e0, e1, e2]
+    end
+
+    [edges, faces_signed]
+  end
+
+  def signed_edge_index(edge_map, a, b)
+    va, vb = [a, b].minmax
+    idx = edge_map["#{va}|#{vb}"]
+    raise "edge not found for #{a}-#{b}" unless idx
+
+    same_dir = (a == va) && (b == vb)
+    same_dir ? idx : -(idx + 1)
   end
 
   def to_s
     line = @parent_node_name ? "createNode mesh -name \"#{@node_name}\" -parent \"#{@parent_node_name}\";" : "createNode transform -name \"#{@node_name}\";"
     str = ''
     str += line
+    # str += "\n\tsetAttr \".opposite\" yes;"
     str += "\n\tsetAttr -keyable off \".visibility\";"
     str += "\n\tsetAttr -size 2 \".instObjGroups[0].objectGroups\";"
     str += "\n\tsetAttr \".instObjGroups[0].objectGroups[0].objectGrpCompList\" -type \"componentList\" 0;"
     str += "\n\tsetAttr \".instObjGroups[0].objectGroups[1].objectGrpCompList\" -type \"componentList\" 1 \"f[0:#{@face.size - 1}]\";"
 
     # Вершины
-    str += "\n\tsetAttr -size #{@vrts.size} \".vrts[0:#{@vrts.size - 1}]\"  \t\t#{@vrts.map { |x, y, z| "#{x} #{y} #{z}" }.join("\t")};"
+    str += "\n\tsetAttr -size #{@vrts.size} \".vrts[0:#{@vrts.size - 1}]\"  \t\t#{@vrts.map do |x, y, z|
+      "#{x} #{y} #{z}"
+    end.join("\t")};"
 
     # Рёбра
-    str += "\n\tsetAttr -size #{@edge.size} \".edge[0:#{@edge.size - 1}]\"  \t\t#{@edge.map { |x, y, z| "#{x} #{y} #{z}" }.join("\t")};"
+    str += "\n\tsetAttr -size #{@edge.size} \".edge[0:#{@edge.size - 1}]\"  \t\t#{@edge.map do |x, y, z|
+      "#{x} #{y} #{z}"
+    end.join("\t")};"
 
-    # UV-пул (.uvpt) — важно: -type "float2"
+    # UV-пул (.uvpt) — ВАЖНО: пишем ровно по числу вершин; значения НЕ клампим (# FIX)
     str += "\n\tsetAttr -size #{@uvpt.size} \".uvpt[0:#{@uvpt.size - 1}]\" -type \"float2\"\t\t" \
            "#{@uvpt.map { |u, v| "#{u} #{v}" }.each_slice(6).map { |chunk| chunk.join('   ') }.join("\t")};"
 
-    # polyFaces: f ... и mf <ровно N индексов>
+    # polyFaces: f ... и mf (uv-индексы совпадают с индексами вершин)
     face_lines = []
     @face.each_with_index do |edges_triplet, i|
-      # "f 3 e1 e2 e3"
       f_part = "f 3 #{edges_triplet.join(' ')}"
-
-      # Для mf нам нужны uv-индексы уголков. Берём вершины из исходного ibuf[i] (порядок per-corner)
-      tri = @ibuf[i]
-      uv_idx = tri.map { |v| @uv_index_of_vertex[v] } # тут просто [v1, v2, v3]
-
-      # "mf u1 u2 u3" (без uv-сета и без count — как у тебя)
+      tri = @ibuf[i] # [v0, v1, v2] — порядок НЕ меняли (# FIX)
+      uv_idx = tri.map { |v| @uv_index_of_vertex[v] }
       mf_part = "mf 3 #{uv_idx.join(' ')}"
-
       face_lines << "\t\t#{f_part}   #{mf_part}"
-      # face_lines << "\t\t#{f_part}  "
     end
 
     str += "\n\tsetAttr -size #{@face.size} \".face[0:#{@face.size - 1}]\" -type \"polyFaces\"\n#{face_lines.join(" \n")};"
+
+    str << "\n"
+    @materials.each do |material|
+      # === Материал ===
+      str << %(createNode lambert -name "#{material[:mat_name]}";\n)
+      str << %(\tsetAttr ".color" -type "float3" #{material[:r]} #{material[:g]} #{material[:b]} ;\n)
+      str << %(\tsetAttr ".transparency" -type "float3" #{material[:t]} #{material[:t]} #{material[:t]} ;\n)
+      str << %(\tsetAttr ".diffuse" 1;\n)
+      str << %(\tsetAttr ".translucence" 0;\n)
+      str << %(\tsetAttr ".ambientColor" -type "float3" 0 0 0;\n)
+
+      # === shadingEngine ===
+      str << %(createNode shadingEngine -name "#{material[:sg_name]}";\n)
+      str << %(\tsetAttr ".ihi" 0;\n)
+      str << %(\tsetAttr ".renderableOnly" yes;\n)
+      str << %(connectAttr "#{material[:mat_name]}.outColor" "#{material[:sg_name]}.surfaceShader";\n)
+
+      # === узел размещения UV ===
+      # str << %(createNode place2dTexture -n "#{mat_name}_place2d";\n)
+      # str << %(    setAttr "#{mat_name}_place2d.repeatU" #{repeatU};\n)
+      # str << %(    setAttr "#{mat_name}_place2d.repeatV" #{repeatV};\n)
+      # str << %(    setAttr "#{mat_name}_place2d.mirrorU" #{mirrorU};\n)
+      # str << %(    setAttr "#{mat_name}_place2d.mirrorV" #{mirrorV};\n)
+      # str << %(    setAttr "#{mat_name}_place2d.rotateUV" #{rotateUV};\n)
+
+      # === назначаем shadingEngine на меш ===
+      str << %(connectAttr "#{@node_name}.instObjGroups" "#{material[:sg_name]}.dagSetMembers" -nextAvailable;\n)
+    end
     str
   end
 
-  # --------- твои утилиты без изменений ---------
+  # --------- утилиты ---------
 
-  def generate_edges_from_ibuf(ibuf)
-    edges = []
-    ibuf.each do |buf|
-      buf.each_cons(2) do |a, b|
-        edges.push([a, b]) if find_edges_index(edges, [a, b]).nil?
-      end
-      edges.push([buf.last, buf.first]) if find_edges_index(edges, [buf.last, buf.first]).nil?
-    end
-    edges
-  end
+  # def generate_edges_from_ibuf(ibuf)
+  #   edges = []
+  #   ibuf.each do |buf|
+  #     buf.each_cons(2) do |a, b|
+  #       edges.push([a, b]) if find_edges_index(edges, [a, b]).nil?
+  #     end
+  #     edges.push([buf.last, buf.first]) if find_edges_index(edges, [buf.last, buf.first]).nil?
+  #   end
+  #   edges
+  # end
 
-  def find_edges_index(edges, value)
-    v1 = nil
-    v2 = nil
-    edges.each_with_index do |edge, index|
-      if edge == value
-        v1 = index
-        break
-      end
-    end
+  # def find_edges_index(edges, value)
+  #   v1 = nil
+  #   v2 = nil
+  #   edges.each_with_index do |edge, index|
+  #     if edge == value
+  #       v1 = index
+  #       break
+  #     end
+  #   end
 
-    reverse_value = value.reverse
-    edges.each_with_index do |edge, index|
-      if edge == reverse_value
-        return -(index + 1)
-      end
-    end
+  #   reverse_value = value.reverse
+  #   edges.each_with_index do |edge, index|
+  #     return -(index + 1) if edge == reverse_value
+  #   end
 
-    return nil if v1.nil? && v2.nil?
-    return v1 unless v1.nil?
-    v2
-  end
+  #   return nil if v1.nil? && v2.nil?
+  #   return v1 unless v1.nil?
 
-  def generate_faces_from_edges_an_ibuf(edges, ibuf)
-    result = []
-    ibuf.each do |buf|
-      v1, v2, v3 = buf
-      i1 = find_edges_index(edges, [v1, v2])
-      i2 = find_edges_index(edges, [v2, v3])
-      i3 = find_edges_index(edges, [v3, v1])
-      result << [i1, i2, i3]
-    end
-    result
-  end
+  #   v2
+  # end
 
-
+  # def generate_faces_from_edges_an_ibuf(edges, ibuf)
+  #   result = []
+  #   ibuf.each do |buf|
+  #     v1, v2, v3 = buf
+  #     i1 = find_edges_index(edges, [v1, v2])
+  #     i2 = find_edges_index(edges, [v2, v3])
+  #     i3 = find_edges_index(edges, [v3, v1])
+  #     result << [i1, i2, i3]
+  #   end
+  #   result
+  # end
 end
 
 def model_to_maya(items)
-  str = "//Maya ASCII 2.5 scene"
+  str = '//Maya ASCII 2.5 scene'
   str += "\nrequires maya \"2.5\";"
   str += "\ncurrentUnit -linear centimeter -angle degree -time film;"
   str += NmfJsonToMaya.new(items).nodes.join("\n")
@@ -356,7 +373,7 @@ if ARGV.length < 2
 end
 
 input_path, output_path = ARGV
-nodes = JSON.parse(File.read(input_path),  symbolize_names: true)
+nodes = JSON.parse(File.read(input_path), symbolize_names: true)
 scene = model_to_maya(nodes)
 
 File.open(output_path, 'w:utf-8') do |io|
